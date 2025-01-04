fe3e7daaf7cf975cf1857544f92efb800f60ccb1
Guru <rugiguru@gmail.com>
2025-01-04 16:49:56 +0530

    Added soln

---
+++ src/patterns/twoPointer/SumOfSqSoln.java
@@ -0,0 +1,23 @@
+package patterns.twoPointer;
+
+public class SumOfSqSoln {
+
+    public boolean judgeSquareSum(int c) {
+        long low = 0;
+        long high = (long) Math.sqrt(c);
+
+        while (low <= high){
+            long current = low*low + high*high;
+
+            if(current < c){
+                low++;
+            }
+            else if(current > c){
+                high--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
