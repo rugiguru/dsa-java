b732851a48b2f1dac2a5b256a2c7f478c8c07db5
Guru <rugiguru@gmail.com>
2025-01-06 18:58:10 +0530

    Added soln

---
+++ src/patterns/twoPointer/ContainerMostWaterSoln.java
@@ -0,0 +1,22 @@
+package patterns.twoPointer;
+
+public class ContainerMostWaterSoln {
+
+    public int maxArea(int[] height) {
+
+        int res = Integer.MIN_VALUE;
+        int n = height.length;
+        int i =0, j = n-1;
+
+        while(i < j){
+            int water = (j-i) * Math.min(height[i], height[j]);
+            res = Math.max(res, water);
+            if(height[i] < height[j]){
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return res;
+    }
+}
