a4172893acc4db5f82853d0d43da29e67b53e3df
Guru <rugiguru@gmail.com>
2025-01-06 19:56:16 +0530

    Added soln

---
+++ src/patterns/twoPointer/SentencepaliandromeSoln.java
@@ -0,0 +1,18 @@
+package patterns.twoPointer;
+
+public class SentencepaliandromeSoln {
+    public boolean isPalindrome(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)){
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        if(sb.toString().equals(sb.reverse().toString())){
+            return true;
+        }
+        return false;
+    }
+}
