87e8105d45e2f7fb8440fbf8a4e58dccbf322f66
Guru <rugiguru@gmail.com>
2025-04-15 20:56:46 +0530

    Added soln for remnove nthe node

--- src/patterns/twoPointer/slowAndFast/LinkedListCycle.java
+++ src/patterns/twoPointer/slowAndFast/LinkedListCycle.java
@@ -43,4 +43,29 @@ public class LinkedListCycle{
         }
         return false;
     }
+
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for(int i=1; i <= n+1; i++){
+            fast = fast.next;
+        }
+
+        while(fast !=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+
+
+    }
 }
